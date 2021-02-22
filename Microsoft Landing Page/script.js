732e2812ad877cbd38d361e994daeb935cb6843e
sandipswain <swainsandip9@gmail.com>
2021-02-22 12:20:14 +0530

    Prevented default

--- Microsoft Landing Page/script.js
+++ Microsoft Landing Page/script.js
@@ -1,3 +1,4 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
+document.querySelector(".menu-btn").addEventListener("click", (e) => {
+  e.preventDefault();
   document.querySelector(".main-menu").classList.toggle("show");
 });
